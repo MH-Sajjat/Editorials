8168213143756dd870d17c27687e53523d6eff07
MH-Sajjat <78714514+MH-Sajjat@users.noreply.github.com>
2021-10-31 21:06:57 +0600

    Update stack.cpp

--- data structure lab/stack.cpp
+++ data structure lab/stack.cpp
@@ -1,46 +1,63 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class Stack {
-	struct Node {
-		int val;
-		Node* next;
-		Node(int x) {
-			val = x;
-			next = NULL;
-		}
-	};
-	Node* topRoot = NULL;
+template <typename T> class Stack {
+    struct Node {
+        T val;
+        Node* next;
+        Node(T x) {
+            val = x;
+            next = NULL;
+        }
+    };
+    Node* topRoot = NULL;
+    int sz = 0;
+
 public:
-	Stack() {
-		topRoot = NULL;
-	}
+    Stack() {
+        topRoot = NULL;
+    }
 
-	void push(int x) {
-		Node* newNode = new Node(x);
-		newNode->next = topRoot;
-		topRoot = newNode;
-	}
+    void push(T x) {
+        Node* newNode = new Node(x);
+        newNode->next = topRoot;
+        topRoot = newNode;
+        sz++;
+    }
 
-	int top() {
-		return topRoot->val;
-	}
+    bool isEmpty() {
+        if (sz) return 1;
+        return 0;
+    }
 
-	void pop() {
-		Node* temp = topRoot;
-		topRoot = topRoot->next;
-		delete(temp);
-	}
+    T top() {
+        if (sz == 0) {
+            cout << "stack is empty" << endl;
+            return -1;
+        }
+        return topRoot->val;
+    }
+
+    void pop() {
+        if (sz == 0) {
+            cout << "stack is empty" << endl;
+            return;
+        }
+        Node* temp = topRoot;
+        topRoot = topRoot->next;
+        delete(temp);
+        sz--;
+    }
 };
 
 int main() {
-	Stack st;
-	st.push(5);
-	st.push(4);
-	st.push(2);
-	st.push(55);
-	st.pop();
-	st.pop();
-	cout << st.top() << endl;
-	return 0;
+    Stack<int> st;
+    st.push(5);
+    st.push(4);
+    st.push(2);
+    st.push(55);
+    st.pop();
+    st.pop();
+    cout << st.top() << endl;
+    return 0;
 }
