f1342e2e42ee52e436daedbdd98c318f2ff400eb
MH-Sajjat <78714514+MH-Sajjat@users.noreply.github.com>
2021-11-01 07:11:01 +0600

    Update bubble_sort.cpp

--- data structure lab/bubble_sort.cpp
+++ data structure lab/bubble_sort.cpp
@@ -1,27 +1,31 @@
 #include <bits/stdc++.h>
 using  namespace std;
 
-void bubbleSort(int ar[], int n) {
-	for (int i = 0; i < (n - 1); ++i) {
-		for (int j = 0; j < n - 1 - i; ++j) {
-			if (ar[j] > ar[j + 1]) {
-				swap(ar[j], ar[j + 1]);
-			}
-		}
-	}
+template <typename type> void bubbleSort(type ar[], int n) {
+    for (int i = 0; i < (n - 1); ++i) {
+        for (int j = 0; j < n - 1 - i; ++j) {
+            if (ar[j] > ar[j + 1]) {
+                swap(ar[j], ar[j + 1]);
+            }
+        }
+    }
 }
 
 int main() {
-	int n;
-	cin >> n;
-	int ar[n];
-	for (int i = 0; i < n; ++i) {
-		cin >> ar[i];
-	}
-	bubbleSort(ar, n);
-	for (int i = 0; i < n; ++i) {
-		cout << ar[i] << " ";
-	}
-	cout << endl;
-	return 0;
+    int n;
+    cout << "Enter the number of element: ";
+    cin >> n;
+    int nums[n];
+    cout << "Enter the elements: ";
+    for (int i = 0; i < n; ++i) {
+        cin >> nums[i];
+    }
+
+    bubbleSort(nums, n);
+    cout << "Sorted array:";
+    for (int i = 0; i < n; ++i) {
+        cout << " " << nums[i];
+    }
+    cout << endl;
+    return 0;
 }
